b7bb7a0b865b8c8e4fe3c1fc68d4122abfc52609
Simon Cockell <sjcockell@gmail.com>
2015-07-10 17:06:30 +0100

    Publications page drafted

--- _site/publications/compile_pub.sh
+++ _site/publications/compile_pub.sh
@@ -1,2 +1,6 @@
 pandoc --filter pandoc-citeproc --bibliography publications.bib --csl custom-author-date.csl -f markdown -t html --ascii biblio.md -o biblio.html
-cat header.md biblio.html >> index.md
+pandoc --filter pandoc-citeproc --bibliography publications.bib --csl custom-author-date.csl -f markdown -t html --ascii acknowledged.md -o acknowledged.html
+cat header.md biblio.html > index.md
+echo '<h2>Acknowledged in</h2>' >> index.md
+echo '<p>The contribution of the Bioinformatics Support Unit has also been acknowledged in the following publications:</p>' >> index.md
+cat acknowledged.html >> index.md
---
+++ publications/acknowledged.md
@@ -0,0 +1,4 @@
+---
+nocite: |
+    @booth_handlebar:_2007, @hancock_maxdload2_2005, @lister_interfacing_2009, @karavolos_adrenaline_2008, @zeybel_multigenerational_2012, @leaver_life_2009, @cox_archaebacterial_2008, @noel_trichomonas_2010
+...

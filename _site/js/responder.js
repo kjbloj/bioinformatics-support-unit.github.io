cb7add7a4874f7e66e7b091a9af1bb99273e47ae
Simon Cockell <sjcockell@gmail.com>
2015-07-24 16:07:07 +0100

    Fix for mobile div clickys?

--- _site/js/responder.js
+++ _site/js/responder.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
         //window.open('mailto:bsu-support@ncl.ac.uk?subject=website%20form%20query&body='+$("#message-body").val());
         window.location.href = 'mailto:bsu-support@ncl.ac.uk?subject=website%20form%20query&body='+$("#message-body").val();
     });
+    $(".ch-item").click(function() {
+        window.location = $(this).find("a").attr("href");
+        return false;
+    });
 });
